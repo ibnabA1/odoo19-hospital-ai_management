23553989c5f8ac5731ea9cd0fcc0b058c5def2e7
ibnabA1 <167630109+ibnabA1@users.noreply.github.com>
2026-03-15 23:10:01 +0300

    Docker files update

--- docker/odoo19/addons/cron_demo/models/models.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class CustomRecord(models.Model):
-    _name= 'custom.record'
-    _description= 'Custom Record'
-
-    name = fields.Char(string= 'Record Name', required= True)
-    status = fields.Selection([('draft', 'Draft'),('completed','Completed')], default= 'draft')
-    
